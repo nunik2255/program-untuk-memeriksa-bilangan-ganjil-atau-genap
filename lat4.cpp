2b12c9c414f253bbfb0feb63d740962d30260300
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:56:04 +0700

    Add files via upload

---
+++ lat4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int bilangan;
+cout<<"masukan bilangan"<<endl;
+cin>>bilangan;
+if(bilangan<0)
+    cout<<"bilangan negatif"<<endl;
+else
+    cout<<"bilangan positif"<<endl;
+return 0;
+}
